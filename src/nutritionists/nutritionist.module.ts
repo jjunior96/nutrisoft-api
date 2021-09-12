50e03bf47acda7857ff6e98fa639c5b4ac114da9
Igor Rodrigues <igorsteixeira94@gmail.com>
2021-09-11 22:50:43 -0300

    Clean up files

--- src/nutritionists/nutritionist.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { NutritionistsService } from './nutritionist.service';
-import { nutritionistsController } from './nutritionist.controller';
-
-@Module({
-  controllers: [nutritionistsController],
-  providers: [NutritionistsService],
-})
-export class nutritionistsModule {}
